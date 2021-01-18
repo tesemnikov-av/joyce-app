0044068013d10c14a1712f6c2a6a08433cd8bc88
tesemnikov-av <54743742+tesemnikov-av@users.noreply.github.com>
2021-01-18 17:13:49 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# joyce-app
+# joyce-app : Anomaly Detection with Time Series Forecasting
+
+
+This application predicted time series and finds deviations of current values from values predicted by the model. Using the difference in these values, we want to notice the abnormal load.
+
+- Python3
+- Zabbix
+- Statsmodels (ARIMA)
+    
+Grafana | Restricted Demonstration Stand
+------|------
+link        |   [Grafana: Restricted Demonstration Stand](http://10.109.247.95:3000/d/2GCJf21Mk/joyce-restricted-demonstration-stand?from=1606998045019&orgId=1&to=1607936331698&var-hostname=protva14)
+user       |   oper
+password   |   oper
+
+![Exaple1](./pics/example1.png)
+
+
+
+If the difference between the maximum and minimum values is less than threshold (for example 5), 
+then we will predict the average value. Otherwise, the prediction algorithm goes crazy.
+This is quite enough for us to detect anomalies.
+
+![Exaple2](./pics/example2.png)
+ 
+
+![Exaple3](./pics/example3.png)
---
+++ app.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import numpy as np
+import glob
+import time
+import os
+
+import logging
+import configparser
+import base64
+
+from pyzabbix import ZabbixAPI
+from influxdb import DataFrameClient
+import statsmodels.api as sm
+from datetime import datetime
+
+from libs.timeseries import write, Prospector, Quacksalver, StoreKeeper
+from libs.journeyman import rename, split_file_names
+
+config = configparser.ConfigParser()
+config.read('/joyce/app/joyce.conf')
+
+TODAY = time.strftime("%Y-%m-%d %H:%M")
+
+INVERT_METRIC = (config['JOYCE']['INVERT_METRIC']).split(',')
+PATH = config['JOYCE']['PATH']
+
+SEASONS = int(config['JOYCE']['SEASONS'])
+METRICS = (config['JOYCE']['METRICS']).split(',')
+NO_OF_HOURS = int(config['JOYCE']['NO_OF_HOURS'])
+HISTORY_SECOND_LEFT = int(config['ZABBIX']['HISTORY_SECOND_LEFT'])
+ROLLING_MEAN = int(config['JOYCE']['ROLLING_MEAN'])
+SUBGROUP = config['JOYCE']['SUBGROUP']
+
+ZABBIX_HOST = config['ZABBIX']['SERVER']
+ZABBIX_USER = config['ZABBIX']['USER']
+ZABBIX_HASH_PASSWORD = os.environ.get('JOYCE_PASSWORD')
+ZABBIX_PASSWORD = base64.b64decode(ZABBIX_HASH_PASSWORD).decode("utf-8")
+ZABBIX_GROUP_IDS = (config['ZABBIX']['GROUP_IDS']).split(',')
+
+INFLUX_HOST = config['INFLUXDB']['SERVER']
+INFLUX_PORT = int(config['INFLUXDB']['PORT'])
+INFLUX_DATABASE = config['INFLUXDB']['DATABASE']
+
+z = ZabbixAPI(server=ZABBIX_HOST)
+z.login(ZABBIX_USER, ZABBIX_PASSWORD)
+
+hosts_id = pd.DataFrame(z.host.get(groupids=ZABBIX_GROUP_IDS, output=['hostid', 'name'], filter={'status': 0}))
+
+if SUBGROUP != 'False':
+    hosts_id = hosts_id[hosts_id.name.str.contains(SUBGROUP)]
+
+INFLUX_CLIENT = DataFrameClient(INFLUX_HOST, INFLUX_PORT, INFLUX_DATABASE)
+
+prospector = Prospector(history_second_left=HISTORY_SECOND_LEFT, rolling_mean=ROLLING_MEAN)
+quacksalver = Quacksalver(seasons=SEASONS, no_of_hours=NO_OF_HOURS, freq=ROLLING_MEAN)
+storekeeper = StoreKeeper(PATH + 'hostname.clusters', hosts_id)
+
+if __name__ == '__main__':
+
+    for _, host in hosts_id.iterrows():
+        """
+            Get original data from Zabbix
+        """
+        host_id, host_name = host
+
+        metrics = []
+        item_ids = z.item.get(hostids=host_id, output=['itemid', 'name'], filter={'name': METRICS})
+
+        for item in item_ids:
+            # Why load data twice ?
+            prospector.load(host_name, item['itemid'], item['name'])
+
+    """
+        Merge data for clusters nodes
+    """
+    storekeeper.merge()
+
+    original_files = glob.glob(PATH + "logs/*" +  "*original.csv")
+
+    for file in original_files:
+        """
+            Find original files and make predictions
+        """
+        hostname, item_id, metric, _ = split_file_names(file)
+        df = pd.read_csv(file)
+
+        quacksalver.forecast(hostname, df, item_id, metric)
+
